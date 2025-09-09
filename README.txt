fe9062db6d2cd1069911016345069de8758b7bc6
Fareen Samad <fareensamad@Fareens-MacBook-Air-2.local>
2025-09-09 13:46:22 -0500

    Fixed testNonempty()

--- README.txt
+++ README.txt
@@ -2,6 +2,5 @@ For TestIterator.java, the setup() method, both ArrayList and LinkedList impleme
 interface, so the test results are the same. The difference is in performance: ArrayList is
 faster for random access (get by index), while LinkedList is better for insertions and removals
 in the middle. For these small tests, there is no major difference. However, ArrayList was
-slightly faster.
-
+slightly faster when I ran the test multiple times.
 
--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -22,7 +22,7 @@ public class TestIterator {
   public void setUp() throws Exception {
     list = new ArrayList<Integer>();
     // list = new LinkedList<Integer>();
-    // TODO Question: Also try with a LinkedList - does it make any difference?
+    // TODO Question: Also try with a LinkedList - does it make any difference? (DONE)
   }
 
   @After
@@ -48,19 +48,19 @@ public class TestIterator {
     final var i = list.iterator();
     assertTrue(i.hasNext());
     assertEquals(33, i.next().intValue());
-    // TODO fix the expected values in the assertions below
+    // TODO fix the expected values in the assertions below (DDNE)
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(44, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(55, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(77, i.next().intValue());
     assertTrue(i.hasNext());
-    assertEquals(0, i.next().intValue());
+    assertEquals(66, i.next().intValue());
     assertFalse(i.hasNext());
   }
 
