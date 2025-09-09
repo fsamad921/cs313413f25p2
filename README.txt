955504610215a4f1f345a55c060f745f1625fd9d
Fareen Samad <fareensamad@Fareens-MacBook-Air-2.local>
2025-09-09 14:04:49 -0500

    Fixed testRemove()

--- README.txt
+++ README.txt
@@ -4,3 +4,8 @@ faster for random access (get by index), while LinkedList is better for insertio
 in the middle. For these small tests, there is no major difference. However, ArrayList was
 slightly faster when I ran the test multiple times.
 
+TODO Question: What happens if you use list.remove(Integer.valueOf(77))?
+When trying to do this approach, it actually has a hard time running the test and threw an
+exception, taking much longer than the given code we have already. I tried running the
+i.remove which was much quicker and then the list.remove(Integer.valueOf(77)) which was much
+slower and eventually I cancelled running the test because of how long it was taking.
--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -76,13 +76,15 @@ public class TestIterator {
     final var i = list.iterator();
     while (i.hasNext()) {
       if (i.next() == 77) {
-        i.remove(); // TODO Question: What happens if you use list.remove(Integer.valueOf(77))?
+        i.remove(); // TODO Question: What happens if you use list.remove(Integer.valueOf(77))? (DONE)
+          // list.remove(Integer.valueOf(77)); Throws exception
       }
     }
-    // TODO using assertEquals and List.of, express which values are left in the list
+    // TODO using assertEquals and List.of, express which values are left in the list (DONE)
+      assertEquals(List.of(33, 44, 55, 66), list);
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
-    fail("Not yet implemented"); // remove this line when done
+    // fail("Not yet implemented"); // remove this line when done
   }
 
   @Test
